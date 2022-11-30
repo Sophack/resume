bbcb378f6e7263d80e7c1b5289d48d03393a649c
Saraf Nawar <s.999895831@gmail.com>
2022-11-30 03:51:20 -0500

    colour change

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var isDark = true;
 
 themeButtonEl.on('click', function () {
     if (isDark) {
-      $('body').css({ 'background-color': '#d9e9e8', color: '#1a1a1a' });
+      $('body').css({ 'background-color': '#4192ada2', color: '#1a1a1a' });
       isDark = !isDark;
     } else {
       $('body').css({ 'background-color': '#1a1a1a', color: '#d9e9e8' });
