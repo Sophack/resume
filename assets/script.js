a18cc7ae93048e72307ce7e799b8abe14d2a3e2e
Sophack <114966651+Sophack@users.noreply.github.com>
2023-01-05 22:12:56 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,15 @@ var isDark = true;
 
 themeButtonEl.on('click', function () {
     if (isDark) {
-      $('body').css({ 'background-color': '#d9e9e8', color: '#1a1a1a' });
+      $('body').css({ 'background-color': '#4192ada2', color: '#1a1a1a' });
       isDark = !isDark;
     } else {
-      $('body').css({ 'background-color': '#1a1a1a', color: '#d9e9e8' });
+      $('body').css({ 'background-color': '#1a1a1a', color: '#4363b9' });
       isDark = !isDark;
     }
   });
+
+  //when isDark = false, color doesnt change
+  //when isDark = true, the else statement happens
+
+  
